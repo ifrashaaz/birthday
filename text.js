8ebdbc78e44ccbb1200a5ea8c4a64005f9b17e52
Ifra Shaaz <ifrashaaz62@gmail.com>
2025-12-23 22:54:59 +0530

    birthday wish

---
+++ text.js
@@ -0,0 +1,21 @@
+ window.onload = function() {
+            var audio = document.getElementById("myAudio");
+            audio.muted = false;
+            audio.play().catch(function(e) {
+                console.log('Audio playback failed:', e);
+            });
+
+        @media screen and (max-width: 658px)    {
+  p {
+    font-weight: 600;
+    font-size: 12vw;
+    text-align: center;
+}
+}
+ #music-container {
+    position: absolute;
+    top: 0;
+    left: 0;
+    display: none;
+  }
+  
